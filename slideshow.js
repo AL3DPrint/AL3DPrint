65d7c2b8f24c97274fc92d0b0d8b5fa02b1dec69
AL3DPrint <andre.lohka@gmail.com>
2026-03-13 18:51:33 +0100

    Create slideshow.js

---
+++ slideshow.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    const images = document.querySelectorAll(".slideshow img");
+    let index = 0;
+
+    setInterval(() => {
+        images[index].classList.remove("active");
+        index = (index + 1) % images.length;
+        images[index].classList.add("active");
+    }, 2000);
+});
